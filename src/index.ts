0c2cebb4159e557fdd5f3da088979e80663b7532
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2024-02-26 22:24:13 +0300

    handle  Navigation

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,38 @@ class generateInput {
             generateInput.InputsContainer.appendChild(tryDiv);
         }
         // console.log(generateInput.InputsContainer.children[0].children[1]);
+        // focus on first element in the Try
         (generateInput.InputsContainer.children[0].children[1] as HTMLInputElement).focus();
+        // Disable All inputs Except first one
+        const inputInDisabledDiv: NodeListOf<HTMLInputElement> = document.querySelectorAll(".disabled-inputs input");
+        // console.log(inputInDisabledDiv);
+        inputInDisabledDiv.forEach((input) => (input.disabled = true));
+
+        const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
+
+        inputs.forEach((input, index) => {
+            input.addEventListener("input", function () {
+                this.value = this.value.toUpperCase();
+                // console.log(index);
+
+                const nextInput = inputs[index + 1];
+                nextInput?.focus();
+            });
+
+            input.addEventListener("keydown", function (event) {
+                console.log(event);
+                const currentIndex = Array.from(inputs).indexOf(event.target as HTMLInputElement);
+                // console.log(currentIndex);
+                if (event.key === "ArrowRight") {
+                    const nextInput = currentIndex + 1;
+                    if(nextInput< inputs.length) inputs[nextInput].focus();
+                }
+                if (event.key === "ArrowLeft") {
+                    const preInput = currentIndex - 1;
+                    if(preInput >= 0) inputs[preInput].focus();
+                }
+            })
+        })
     }
 }
 const inputGenerator = new generateInput();
